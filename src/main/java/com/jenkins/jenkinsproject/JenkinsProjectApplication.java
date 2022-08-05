960a2c8e155efe12fe06fd12145378c35369544b
tolulopeoredein <oredeintolulope@gmail.com>
2022-08-05 15:05:24 +0100

    next commit
    next commit

--- src/main/java/com/jenkins/jenkinsproject/JenkinsProjectApplication.java
+++ src/main/java/com/jenkins/jenkinsproject/JenkinsProjectApplication.java
@@ -2,7 +2,11 @@ package com.jenkins.jenkinsproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class JenkinsProjectApplication {
 
@@ -10,4 +14,12 @@ public class JenkinsProjectApplication {
 		SpringApplication.run(JenkinsProjectApplication.class, args);
 	}
 
+
+	@RequestMapping("/")
+	public String message(){
+
+		return "Thank You Jesus";
+	}
 }
+
+
